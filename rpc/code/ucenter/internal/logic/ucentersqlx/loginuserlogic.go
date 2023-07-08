6596545ac6488b3fed170fc7add8e1170c98ad73
“ximuqi” <ximuqi@163.com>
2023-07-09 04:00:46 +0800

    feat：api、rpc调整配置
    1. 调整超时时间：将rpc和api的yaml中将超时时间统一调整为600000毫秒（即10分钟）
    2. rpc的yaml中关闭grpc调试模式
    3. rpc的登录处理逻辑屏蔽模拟耗时操作
    4. api的yaml中将`MaxBytes`从1MB改为10000MB

--- rpc/code/ucenter/internal/logic/ucentersqlx/loginuserlogic.go
+++ rpc/code/ucenter/internal/logic/ucentersqlx/loginuserlogic.go
@@ -31,9 +31,9 @@ func (l *LoginUserLogic) LoginUser(in *ucenter.User) (*ucenter.UserLoginResp, er
 	// todo: add your logic here and delete this line
 	//return &ucenter.UserLoginResp{}, nil
 
-	//模拟耗时 20秒钟
-	sleepTime := 20 * time.Second
-	time.Sleep(sleepTime)
+	////模拟耗时 20秒钟
+	//sleepTime := 20 * time.Second
+	//time.Sleep(sleepTime)
 	return l.LoginSuccess(in)
 }
 
