8dbec6acd5963ff844d9a11467168a1bb6608b2d
“ximuqi” <ximuqi@163.com>
2023-07-11 07:32:17 +0800

    feat：rpc服务拦截器初次使用

--- rpc/code/ucenter/ucenter.go
+++ rpc/code/ucenter/ucenter.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	filestorageServer "go-zero-micro/rpc/code/ucenter/internal/server/filestorage"
@@ -42,6 +43,28 @@ func main() {
 	MaxFileSize := int(c.UploadFile.MaxFileSize)
 	//调整RPC服务端收到的消息体大小限制
 	s.AddOptions(grpc.MaxRecvMsgSize(MaxFileSize))
+
+	//拦截器
+	s.AddUnaryInterceptors(func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		fmt.Printf("RpcServerInterceptor1 ====> Start \n")
+		fmt.Printf("req =====================> %+v \n", req)
+		fmt.Printf("info =====================> %+v \n", info)
+		resp, err = handler(ctx, req)
+		fmt.Printf("resp =====================> %+v \n", resp)
+		fmt.Printf("RpcServerInterceptor1 ====> End \n")
+		return resp, err
+	})
+
+	//拦截器
+	s.AddUnaryInterceptors(func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		fmt.Printf("RpcServerInterceptor2 ====> Start \n")
+		fmt.Printf("req =====================> %+v \n", req)
+		fmt.Printf("info =====================> %+v \n", info)
+		resp, err = handler(ctx, req)
+		fmt.Printf("resp =====================> %+v \n", resp)
+		fmt.Printf("RpcServerInterceptor2 ====> End \n")
+		return resp, err
+	})
 
 	defer s.Stop()
 
