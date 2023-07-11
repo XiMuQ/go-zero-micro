bff9f8565f29069ec655cb24065d0a8415a3a473
“ximuqi” <ximuqi@163.com>
2023-07-11 09:50:20 +0800

    feat：服务间通过metadata传递数据

--- common/interceptor/apinterceptor.go
+++ common/interceptor/apinterceptor.go
@@ -3,7 +3,9 @@ package interceptor
 import (
 	"context"
 	"fmt"
+	"go-zero-micro/common/utils"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
 // RpcClientInterceptor1 rpc的客户端拦截器
@@ -23,6 +25,12 @@ func RpcClientInterceptor1(ctx context.Context, method string, req, reply interf
 func RpcClientInterceptor2(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
 	fmt.Printf("RpcClientInterceptor2 ====> Start \n")
 	fmt.Printf("req =====================> %+v \n", req)
+
+	mapData := map[string]string{}
+	mapData["userId"] = utils.Base64Encode("111")
+	mapData["userName"] = utils.Base64Encode("哈哈哈")
+	md := metadata.New(mapData)
+	ctx = metadata.NewOutgoingContext(ctx, md)
 
 	err := invoker(ctx, method, req, reply, cc, opts...)
 	fmt.Printf("RpcClientInterceptor2 ====> End \n")
--- common/interceptor/rpcinterceptor.go
+++ common/interceptor/rpcinterceptor.go
@@ -3,7 +3,10 @@ package interceptor
 import (
 	"context"
 	"fmt"
+	"go-zero-micro/common/utils"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+	"strconv"
 )
 
 // RpcServerInterceptor1 rpc的服务端拦截器
@@ -22,6 +25,21 @@ func RpcServerInterceptor2(ctx context.Context, req interface{}, info *grpc.Unar
 	fmt.Printf("RpcServerInterceptor2 ====> Start \n")
 	fmt.Printf("req =====================> %+v \n", req)
 	fmt.Printf("info =====================> %+v \n", info)
+
+	if md, ok := metadata.FromIncomingContext(ctx); ok {
+		tmp := md.Get("userId")
+		if len(tmp) > 0 {
+			userId, _ := utils.Base64Decode(tmp[0])
+			uid, _ := strconv.ParseInt(userId, 10, 64)
+			fmt.Printf("userId：%d\n", uid)
+		}
+		uname := md.Get("userName")
+		if len(tmp) > 0 {
+			userName, _ := utils.Base64Decode(uname[0])
+			fmt.Printf("userName：%s\n", userName)
+		}
+	}
+
 	resp, err = handler(ctx, req)
 	fmt.Printf("resp =====================> %+v \n", resp)
 	fmt.Printf("RpcServerInterceptor2 ====> End \n")
