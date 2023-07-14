1479550bb920c1da74ec2bf9ffaa330f18346915
“ximuqi” <ximuqi@163.com>
2023-07-14 09:11:03 +0800

    feat：api服务实现添加用户

--- api/code/ucenterapi/internal/logic/ucenter/adduserlogic.go
+++ api/code/ucenterapi/internal/logic/ucenter/adduserlogic.go
@@ -2,6 +2,9 @@ package ucenter
 
 import (
 	"context"
+	"github.com/jinzhu/copier"
+	"go-zero-micro/common/errorx"
+	"go-zero-micro/rpc/code/ucenter/ucenter"
 
 	"go-zero-micro/api/code/ucenterapi/internal/svc"
 	"go-zero-micro/api/code/ucenterapi/internal/types"
@@ -24,7 +27,19 @@ func NewAddUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *AddUserLo
 }
 
 func (l *AddUserLogic) AddUser(req *types.UserSimpleModel) (resp *types.BaseModel, err error) {
-	// todo: add your logic here and delete this line
-
-	return
+	//这里没做严格的参数校验
+	if req.Account == "" || req.Username == "" {
+		return nil, errorx.NewDefaultError(errorx.ParamErrorCode)
+	}
+	param := &ucenter.User{}
+	copier.Copy(param, req)
+	rpcRes, err := l.svcCtx.UcenterSqlxRpc.AddUser(l.ctx, param)
+	if err != nil {
+		return nil, errorx.NewDefaultError(errorx.ServerErrorCode)
+	}
+	resp = &types.BaseModel{
+		Id:   rpcRes.Id,
+		Data: "用户添加成功",
+	}
+	return resp, nil
 }
