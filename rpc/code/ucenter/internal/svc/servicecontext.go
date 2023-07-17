75d15f4bcec07794102bb40f8c34ed5d216be6e2
“ximuqi” <ximuqi@163.com>
2023-07-17 17:17:27 +0800

    feat：RPC添加sqlc数据库查询依赖

--- rpc/code/ucenter/internal/svc/servicecontext.go
+++ rpc/code/ucenter/internal/svc/servicecontext.go
@@ -3,6 +3,7 @@ package svc
 import (
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
 	"go-zero-micro/rpc/code/ucenter/internal/config"
+	sqlc_usermodel "go-zero-micro/rpc/database/sqlc/usermodel"
 	sqlx_usermodel "go-zero-micro/rpc/database/sqlx/usermodel"
 )
 
@@ -10,6 +11,9 @@ type ServiceContext struct {
 	Config             config.Config
 	SqlxUsersModel     sqlx_usermodel.ZeroUsersModel
 	SqlxUserInfosModel sqlx_usermodel.ZeroUserInfosModel
+
+	SqlcUsersModel     sqlc_usermodel.ZeroUsersModel
+	SqlcUserInfosModel sqlc_usermodel.ZeroUserInfosModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
@@ -19,5 +23,8 @@ func NewServiceContext(c config.Config) *ServiceContext {
 		Config:             c,
 		SqlxUsersModel:     sqlx_usermodel.NewZeroUsersModel(mysqlConn),
 		SqlxUserInfosModel: sqlx_usermodel.NewZeroUserInfosModel(mysqlConn),
+
+		SqlcUsersModel:     sqlc_usermodel.NewZeroUsersModel(mysqlConn, c.CacheRedis),
+		SqlcUserInfosModel: sqlc_usermodel.NewZeroUserInfosModel(mysqlConn, c.CacheRedis),
 	}
 }
